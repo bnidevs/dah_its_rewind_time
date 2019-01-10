da0cc5d25ea7c2c2abc600964e2b1555401c78df
Vincent Chi <vchi90@stuy.edu>
2019-01-10 14:05:24 -0500

    kewl progress

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 from flask import Flask, redirect, url_for, render_template, session, request, flash, get_flashed_messages
 
-from utils import database as arms, api
+from utils import database, api
 
 
 # instantiate Flask object
@@ -17,7 +17,6 @@ app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 # manage cookies and user data here
-DB_FILE = "data/rewind.db"
 user = None
 
 def setUser(userName):
@@ -28,7 +27,7 @@ def setUser(userName):
 @app.route('/')
 def home():
     if user in session:
-        return render_template('index.html', errors = True, logged_in = True)
+        return render_template('login.html', errors = True, logged_in = True)
     return render_template('index.html', errors = True, logged_in = False)
 
 @app.route('/register')
@@ -48,32 +47,24 @@ def authenticate():
     if user in session:
         return redirect(url_for('home'))
     # instantiates DB_Manager with path to DB_FILE
-    data = arms.DB_Manager(DB_FILE)
     username, password, curr_page = request.form['username'], request.form['password'], request.form['address']
     print(curr_page)
     # LOGGING IN
     if request.form["submit"] == "Login":
-        if username != "" and password != "" and data.verifyUser(username, password):
+        if username != "" and password != "" and database.newuser(username, password):
             session[username] = password
             setUser(username)
-            data.save()
             flash('Successfully logged in!')
             return redirect(curr_page)
-        # user was found in DB but password did not match
-        elif data.findUser(username):
-            flash('Incorrect password!')
-        # user not found in DB at all
         else:
-            flash('Incorrect username!')
-        data.save()
+            flash('Incorrect credentials!')
         return redirect(curr_page)
     # REGISTERING
     else:
         if len(username.strip()) != 0 and not data.findUser(username):
             if len(password.strip()) != 0:
                 # add account to DB
-                data.registerUser(username, password)
-                data.save()
+                database.newuser(username, password)
                 #data.setOut()
                 flash('Successfully registered account for user  "{}"'.format(username))
                 return redirect(url_for('home'))
